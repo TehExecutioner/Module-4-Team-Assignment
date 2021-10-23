1efa18f9cd7c03b044b89206ef06f286cd3952f5
Noah Martinez <91973097+TehExecutioner@users.noreply.github.com>
2021-10-23 17:52:37 -0500

    Add files via upload
    still needs to be worked on

---
+++ Team Project (Draft).py
@@ -0,0 +1,14 @@
+# Team: Jamie, Lakiesha, Noah
+# Objective:
+
+#Steps to complete program
+
+#Step 1:
+"?"
+Choose= int(input("Please choose your option from the list below:"))
+print("1. Sleep
+print("2. Watch Youtube
+print("3. Call Friend
+print("4.
+print("5.
+print("0. Exit
